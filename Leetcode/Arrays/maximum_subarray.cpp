e9f76a62b9002fd09a410473202580880155afd3
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-09 07:11:33 +0000

    Fixed kadane algo

--- Leetcode/Arrays/maximum_subarray.cpp
+++ Leetcode/Arrays/maximum_subarray.cpp
@@ -4,7 +4,7 @@ class Solution{
         int maxSubArray(vector<int>& nums) {
             int curr_sum = 0;
             int max_sum = INT_MIN;
-            for(int num: nums){
+            for(int num: nums) {
                 curr_sum = max(num,curr_sum + num);
                 max_sum = max(max_sum,curr_sum);
             }
