c6ca0c74b0e286ca5dd51b346af0303fc911293b
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-10 06:08:25 +0000

    Sorting method in 2 sum

--- Leetcode/Arrays/two_sum.cpp
+++ Leetcode/Arrays/two_sum.cpp
@@ -1,3 +1,8 @@
+/*
+    Hash table
+    Time complexity - O(n)
+    Space complexity - O(n)
+*/
 class Solution {
     public:
         vector<int> intSum(vector<int>& nums, int target) {
@@ -11,4 +16,35 @@ class Solution {
             }
             return {};
         }
+};
+
+/*
+    Sorting
+    Time complexity - O(nlogn)
+    Space complexity - O(n)
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> v;
+        for(int i = 0; i < nums.size(); i++){
+            v.push_back({nums[i], i});
+        }
+        
+        sort(v.begin(), v.end());
+        int i = 0, j = nums.size() - 1;
+        while (i < j) {
+            int curr = v[i].first + v[j].first;
+            if(curr == target) {
+                return  {min(v[i].second, v[j].second),
+                        max(v[i].second, v[j].second)};
+            } else if (curr < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return {};
+    }
 };
