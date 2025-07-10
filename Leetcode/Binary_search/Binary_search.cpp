899cad587e419a57cebd374a587098cdf6d4f508
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-10 10:34:00 +0000

    Intial commit

---
+++ Leetcode/Binary_search/Binary_search.cpp
@@ -0,0 +1,22 @@
+/*
+    https://leetcode.com/problems/binary-search/description/
+    Time complexity-
+    Space complexity-
+*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] < target) {
+                left = mid + 1 ;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
