c887e748699af87e604263d68bbeb34214bf6bcd
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-11 02:22:36 +0000

    Added set solution for valid anagram

--- Leetcode/Arrays/valid_anagram.cpp
+++ Leetcode/Arrays/valid_anagram.cpp
@@ -18,3 +18,29 @@ class Solution{
             return counts == countT;
         }
 };
+
+/*
+    Hash set
+    Time complexity - O(n+m)
+    Space complexity - O(n+m)
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+
+        unordered_map<int, int> count;
+        
+        for(char c: s){
+            count[c]++;
+        }
+        for(char c: t){
+            if(count.find(c) == count.end() || count[c] == 0){
+                return false;
+            }
+            count[c]--;
+        }
+        return true;
+    }
+};
