311c984fdca36ff0d2d9256b923aad52588284a2
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-11 06:36:04 +0000

    Initial commit

--- Leetcode/Two_pointers/valid_palindrome.cpp
+++ Leetcode/Two_pointers/valid_palindrome.cpp
@@ -15,3 +15,31 @@ public:
         return str == string(str.rbegin(), str.rend());
     }
 };
+
+/*
+    Two pointer
+    Time complexity - O(n)
+    Space complexity - O(1)
+*/
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        while(left < right){
+            if(!isalnum(s[left])) {
+                left++;
+                continue;
+            }
+            if(!isalnum(s[right])) {
+                right--;
+                continue;
+            }
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
