3e2692240049520eff2c1cf64af9f57efc8a1506
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-11 03:18:22 +0000

    Intial commit

---
+++ Leetcode/Arrays/top_k_frequent_element.cpp
@@ -0,0 +1,26 @@
+/*
+    https://leetcode.com/problems/top-k-frequent-elements/description/
+    Time complexity - O(nlogn)
+    Space complexity - O(n)
+*/
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        for(int num: nums) {
+            map[num]++;
+        }
+        vector<pair<int, int>> v;
+        for(pair<int, int> p: map) {
+            v.push_back(p);
+        }
+        sort(v.begin(), v.end(),[](pair<int, int> a, pair<int, int> b){
+            return a.second > b.second;
+        });
+        vector<int> res;
+        for(int i = 0; i < k; i++){
+            res.push_back(v[i].first);
+        }
+        return res;
+    }
+};
