5860f64498b0589e3f1dc343e1c755507750ee2f
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-12 04:48:37 +0000

    Added fast_slow sol

--- Leetcode/Linked_list/linked_list_cycle.cpp
+++ Leetcode/Linked_list/linked_list_cycle.cpp
@@ -27,3 +27,35 @@ public:
         return false;
     }
 };
+
+/*
+    Two pointer 
+    Time complexity - O(n)
+    Space complexity - O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
