1a0a8852e7e1ed60d8cddfb99e3f5d0960d85579
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-18 11:40:49 +0000

    Added time complexity

--- Leetcode/Sorting/bubble_sort.cpp
+++ Leetcode/Sorting/bubble_sort.cpp
@@ -1,28 +1,19 @@
-vector<int> bubbleSort(vector<int> arr) {
+/*
+	Time complexity - O(n^2)
+	Space complexity - O(1)
+*/
+vector<int> bubblesort(vector<int> arr) {
 	bool sorted = true;
-	
 	for(int i = 0; i < arr.size() - 1; i++) {
 		for(int j = 0; j < arr.size() - i - 1; j++) {
-			if(arr[j] > arr[j+1]) {
-				swap(arr[j], arr[j+1]);
-				sorted = false;	
+			if(arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				sorted = false;
 			}
 		}
-		if(sorted)
-			return arr;	
-	}
-	return arr;
-}
-vector<int> insertionSort(vector<int> arr) {
-	int j = 0;
-	
-	for(int i = 1; i < arr.size(); i++) {
-		j = i;
-		while(j > 0 && arr[j-1] > arr[j]) {
-			swap(arr[j-1], arr[j]);
-			--j;
+		if(sorted) {
+			return arr;
 		}
 	}
-	
 	return arr;
 }
