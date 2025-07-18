42d5b5b674e6937e83e468f29a467128ce2b50c3
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-18 11:51:20 +0000

    Added time complexity

--- Leetcode/Sorting/selection_sort.cpp
+++ Leetcode/Sorting/selection_sort.cpp
@@ -1,11 +1,13 @@
-
-
+/*
+    Time complexity - O(n^2)
+    Space complexity - O(n)
+*/
 vector<int> selectionSort(vector<int> arr) {
     int min_el;
     for(int i = 0; i < arr.size() - 1; i++) {
         min_el = i;
         for(int j = i+1; j < arr.size(); j++) {
-            if(arr[j] < arr[min_el])
+            if(arr[j] < arr[min_el]) 
                 min_el = j;
         }
         swap(arr[i], arr[min_el]);
