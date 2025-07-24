74c00554bef03826d23be92b48321c42a6cb7df0
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-24 11:29:44 +0000

    Initial commit

---
+++ Leetcode/Heap/kth_largest_element_in_an_array.cpp
@@ -0,0 +1,18 @@
+/*
+    https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+    Time complexity - O(nlogk)
+    Space complexity - O(k)
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for(int num: nums) {
+            minHeap.push(num);
+            if(minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
