4aa9d644a52a51051ebdd06ae2dde43f0b6623ba
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-26 03:26:57 +0000

    Initial commit

---
+++ Leetcode/Heap/last_stone_weight.cpp
@@ -0,0 +1,25 @@
+/*
+    https://leetcode.com/problems/last-stone-weight/description/
+    Time complexity - O(nlogn)
+    Space complexity - O(n)
+*/
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for(int s: stones) {
+            maxHeap.push(s);
+        }
+
+        while (maxHeap.size() > 1) {
+            int first = maxHeap.top();
+            maxHeap.pop();
+            int second = maxHeap.top();
+            maxHeap.pop();
+            if(first > second) {
+                maxHeap.push(first - second);
+            }
+        }
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
