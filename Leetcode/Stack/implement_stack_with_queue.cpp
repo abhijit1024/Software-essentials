22d3bb8e483934561c9e26f6dd70ee122161745c
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-26 05:14:49 +0000

    Initial commit

--- Leetcode/Stack/implement_stack_with_queue.cpp
+++ Leetcode/Stack/implement_stack_with_queue.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {}
+
+    void push(int x) {
+        int s = q.size();
+        q.push(x);
+        for(int i = 0; i < s; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    int top() {
+        return q.front();
+    }
+    bool isEmpty() {
+        return q.empty();
+    }
+};
+
 int main() {
+    MyStack st;
+
+    st.push(10);
+    st.push(20);
+    cout << "Top: " << st.top() << endl; // Should print 20
+    st.pop();
+    cout << "Top after pop: " << st.top() << endl; // Should print 10
 
     return 0;
 }
