136e72d0bbdb70b959c1fab0aefced2f6bef08b4
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-27 03:16:45 +0000

    Initial commit

---
+++ Leetcode/Sliding_window/longest_repeating_character_replacement.cpp
@@ -0,0 +1,31 @@
+/*
+    https://leetcode.com/problems/longest-repeating-character-replacement/description/
+    Time complexity - O(n)
+    Space complexity - O(1)
+*/
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        if(s.empty())
+            return 0;
+        vector<int> dict(128, 0);
+        int flips = k;
+        int max_len = 0;
+        int max_freq = 0;
+
+        int i = 0, j = 0;
+        while(i < s.size()) {
+            ++dict[s[i]];
+            max_freq = max(max_freq, dict[s[i]]);
+            ++i;
+            int flips_req = i - j - max_freq;
+            while(flips_req > k) {
+                --dict[s[j]];
+                ++j;
+                flips_req = i - j - max_freq;
+            }
+            max_len = max(max_len, i - j);
+        }
+        return max_len;
+    }
+};
