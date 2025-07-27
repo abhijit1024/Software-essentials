04e18a52b7c358beabffb913daf020598569ef1b
Abhijit1024 <abhi.decoder1@gmail.com>
2025-07-27 03:18:04 +0000

    Initial commit

---
+++ Leetcode/Sliding_window/longest_substring_without_repeating_charactars.cpp
@@ -0,0 +1,32 @@
+/*  
+    https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+    Time complexity - O(n)
+    Space complexity - O(1)
+*/
+class Solution {
+public:
+        int lengthOfLongestSubstring(string s) {
+        int i = 0, j = 0;
+        vector<int> dict(256, 0);
+        int max_len = 0;
+        bool repeating = false;
+        
+        while(i < s.size()) {
+            ++dict[s[i]];
+            if(dict[s[i]] > 1)
+                repeating = true;
+            ++i;
+           
+            while(repeating) {
+                --dict[s[j]];
+                if(dict[s[j]] >= 1)
+                    repeating = false;
+                ++j;
+            }
+            
+            max_len = max(max_len, i - j);
+        }
+        
+        return max_len;
+    }
+};
