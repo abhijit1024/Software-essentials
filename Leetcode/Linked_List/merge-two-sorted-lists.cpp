4983c2422d2eecfb73df83afca9ba1362a8765cc
Abhijit1024 <abhi.decoder1@gmail.com>
2025-08-19 04:11:53 +0000

    Added min heap soln

--- Leetcode/Linked_List/merge-two-sorted-lists.cpp
+++ Leetcode/Linked_List/merge-two-sorted-lists.cpp
@@ -38,3 +38,47 @@ public:
         return dummy.next;
     }
 };
+/*
+    Min heap
+    Time complexity - O(NlogK)
+    Space complexity - O(k)
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()) return nullptr;
+
+        auto cmp = [](ListNode* a, ListNode* b) {return a->val > b->val;};
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp) > minHeap(cmp);
+        for(ListNode* list: lists) {
+            if(list != nullptr) {
+                minHeap.push(list);
+            }
+        }
+        ListNode* res = new ListNode(0);
+        ListNode* curr = res;
+        while(!minHeap.empty()) {
+            ListNode* node = minHeap.top();
+            minHeap.pop();
+            curr->next = node;
+            curr = curr->next;
+
+            node = node->next;
+            if(node !=nullptr) {
+                minHeap.push(node);
+            }
+        }
+        return res->next;
+    }
+};
