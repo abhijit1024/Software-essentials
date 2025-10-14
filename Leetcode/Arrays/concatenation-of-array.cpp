8bc61ff53f3319322475ec41958793e939262c50
Abhijit1024 <abhi.decoder1@gmail.com>
2025-10-14 02:53:43 +0000

    First commit

---
+++ Leetcode/Arrays/concatenation-of-array.cpp
@@ -0,0 +1,16 @@
+/*
+    https://leetcode.com/problems/concatenation-of-array/description/
+    Time complexity - O(n)
+    Space complexity - O(n)
+*/
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2*n);
+        for(int i = 0; i < n; i++) {
+            ans[i] = ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+};
