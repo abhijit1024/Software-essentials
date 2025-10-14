5b7bc0a14ea138194d917ee541b5e75935020a21
Abhijit1024 <abhi.decoder1@gmail.com>
2025-10-14 03:01:13 +0000

    Added another sol

--- Leetcode/Arrays/concatenation-of-array.cpp
+++ Leetcode/Arrays/concatenation-of-array.cpp
@@ -14,3 +14,19 @@ public:
         return ans;
     }
 };
+/*
+    Time complexity - O(n)
+    Space compleixty - O(n)
+*/
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < 2; i++) {
+            for(int num: nums) {
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
