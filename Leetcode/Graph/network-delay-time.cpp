aa1a82fb819513976c77faeb6ccee5d7f87671aa
Abhijit1024 <abhi.decoder1@gmail.com>
2025-10-27 13:44:52 +0000

    Initial commit

---
+++ Leetcode/Graph/network-delay-time.cpp
@@ -0,0 +1,26 @@
+/*
+    https://leetcode.com/problems/network-delay-time/
+    Time complexity - O(V*E)
+    Space compleixty - O(V)
+*/
+/*
+    Using bellman ford
+*/
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int> dist(n, INT_MAX);
+        dist[k - 1] = 0;
+
+        for(int i = 0; i < n - 1; i++) {
+            for(const auto& time : times) {
+                int u = time[0] - 1, v = time[1] - 1, w = time[2];
+                if(dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+        int maxDist = *max_element(dist.begin(), dist.end());
+        return maxDist == INT_MAX ? -1 : maxDist;
+    }
+};
