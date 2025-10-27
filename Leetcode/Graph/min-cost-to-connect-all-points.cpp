dbba09a60cac8ba9b7ecb80f3b9411f9d44c027e
Abhijit1024 <abhi.decoder1@gmail.com>
2025-10-27 13:51:35 +0000

    Initial commit

---
+++ Leetcode/Graph/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,53 @@
+/*
+    https://leetcode.com/problems/min-cost-to-connect-all-points/description/
+    Time complexity - 
+    Space complexity -
+*/
+/*
+    Using kruskals
+*/
+class DSU {
+public:
+    vector<int> Parent, Size;
+    DSU(int n) : Parent(n + 1), Size(n + 1, 1) {
+        for(int i = 0; i <= n; i++) Parent[i] = i;
+    }
+    int find(int node) {
+        if(Parent[node] != node) {
+            Parent[node] = find(Parent[node]);
+        }
+        return Parent[node];
+    }
+    bool unionSets(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if (pu == pv) return false;
+        if (Size[pu] < Size[pv]) swap(pu, pv);
+        Size[pu] += Size[pv];
+        Parent[pv] = pu;
+        return true;
+    }
+};
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+       int n = points.size();
+       DSU dsu(n);
+       vector<tuple<int, int, int>> edges;
+
+       for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j  < n; j++) {
+            int dist = abs(points[i][0] - points[j][0]) +
+                        abs(points[i][1] - points[j][1]);
+            edges.push_back({dist, i, j});
+        }
+       }
+       sort(edges.begin(), edges.end());
+       int res = 0;
+       for(auto& [dist, u, v] : edges) {
+        if(dsu.unionSets(u, v)) {
+            res += dist;
+        }
+       } 
+       return res;
+    }
+};
