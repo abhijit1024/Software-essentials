fc62088b1ef34e6b927b5cafbfa0014db04feb0d
Abhijit1024 <abhi.decoder1@gmail.com>
2025-10-30 10:40:18 +0000

    Update the if loop

--- Leetcode/Graph/walls-and-gate(teasure-island).cpp
+++ Leetcode/Graph/walls-and-gate(teasure-island).cpp
@@ -11,32 +11,26 @@ public:
         int n = grid[0].size();
 
         queue<pair<int, int>> q;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) 
                     q.push({i, j});
-                }
             }
         }
-
-        vector<vector<int>> dirs = {{-1, 0}, {1, 0},
-                                    {0, -1}, {0, 1}};
-        while (!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
+        vector<pair<int, int>> direction {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        while(!q.empty()) {
+            auto [row, col] = q.front();
             q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int r = row + dirs[i][0];
-                int c = col + dirs[i][1];
-
-                if (r < 0 || r >= m || c < 0 ||
-                    c >= n || grid[r][c] != INT_MAX) {
-                    continue;
-                }
-
-                grid[r][c] = grid[row][col] + 1;
-                q.push({r, c});
+            for(auto [dx, dy] : direction) {
+                int r = row + dx;
+                int c = col + dy;
+                if(r >= 0 && r < m &&
+                    c >= 0 && c < n && grid[r][c] == INT_MAX) {
+                        grid[r][c] = grid[row][col] + 1;
+                        q.push({r, c});
+                    }
             }
         }
     }
